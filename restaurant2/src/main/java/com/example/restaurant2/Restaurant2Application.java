d87ea6892ac5586cee1b0d63f51d6d4142eb10c8
lkai30 <42429958+lkai30@users.noreply.github.com>
2018-08-16 14:23:32 +0800

    Add files via upload

---
+++ restaurant2/src/main/java/com/example/restaurant2/Restaurant2Application.java
@@ -0,0 +1,12 @@
+package com.example.restaurant2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Restaurant2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Restaurant2Application.class, args);
+	}
+}
